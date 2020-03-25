f02796590d05e0711efc23372ce968a03f9a958f
Pasises <45708319+pasises@users.noreply.github.com>
2020-03-25 21:50:44 +0900

    Create Animal Syogi

---
+++ AnimalSyogi/Form1.Designer.cs
@@ -0,0 +1,278 @@
+﻿namespace AnimalSyogi {
+    partial class Form1 {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.resetButton = new System.Windows.Forms.Button();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.errorLabel = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Inset;
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox7, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox8, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox6, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox2, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox3, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox4, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox5, 2, 3);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(444, 525);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.pictureBox7.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox7.Image")));
+            this.pictureBox7.Location = new System.Drawing.Point(5, 395);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(139, 124);
+            this.pictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox7.TabIndex = 10;
+            this.pictureBox7.TabStop = false;
+            this.pictureBox7.Visible = false;
+            this.pictureBox7.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseDown);
+            this.pictureBox7.MouseUp += new System.Windows.Forms.MouseEventHandler(this.picture_MouseUp);
+            // 
+            // pictureBox8
+            // 
+            this.pictureBox8.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.pictureBox8.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox8.Image")));
+            this.pictureBox8.Location = new System.Drawing.Point(152, 265);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(139, 122);
+            this.pictureBox8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox8.TabIndex = 8;
+            this.pictureBox8.TabStop = false;
+            this.pictureBox8.Visible = false;
+            this.pictureBox8.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseDown);
+            this.pictureBox8.MouseUp += new System.Windows.Forms.MouseEventHandler(this.picture_MouseUp);
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.pictureBox6.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox6.Image")));
+            this.pictureBox6.Location = new System.Drawing.Point(152, 395);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(139, 124);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox6.TabIndex = 11;
+            this.pictureBox6.TabStop = false;
+            this.pictureBox6.Visible = false;
+            this.pictureBox6.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseDown);
+            this.pictureBox6.MouseUp += new System.Windows.Forms.MouseEventHandler(this.picture_MouseUp);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(5, 5);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(139, 120);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Visible = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseDown);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.picture_MouseUp);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(152, 5);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(139, 122);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Visible = false;
+            this.pictureBox2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseDown);
+            this.pictureBox2.MouseUp += new System.Windows.Forms.MouseEventHandler(this.picture_MouseUp);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(299, 5);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(140, 122);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Visible = false;
+            this.pictureBox3.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseDown);
+            this.pictureBox3.MouseUp += new System.Windows.Forms.MouseEventHandler(this.picture_MouseUp);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+            this.pictureBox4.Location = new System.Drawing.Point(152, 135);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(139, 122);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 5;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Visible = false;
+            this.pictureBox4.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseDown);
+            this.pictureBox4.MouseUp += new System.Windows.Forms.MouseEventHandler(this.picture_MouseUp);
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.pictureBox5.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox5.Image")));
+            this.pictureBox5.Location = new System.Drawing.Point(299, 395);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(140, 124);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 12;
+            this.pictureBox5.TabStop = false;
+            this.pictureBox5.Visible = false;
+            this.pictureBox5.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseDown);
+            this.pictureBox5.MouseUp += new System.Windows.Forms.MouseEventHandler(this.picture_MouseUp);
+            // 
+            // resetButton
+            // 
+            this.resetButton.Location = new System.Drawing.Point(714, 110);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(127, 73);
+            this.resetButton.TabIndex = 1;
+            this.resetButton.Text = "リセット";
+            this.resetButton.UseVisualStyleBackColor = true;
+            this.resetButton.Click += new System.EventHandler(this.resetButton_Click);
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(462, 17);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 3;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(211, 252);
+            this.tableLayoutPanel2.TabIndex = 2;
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.ColumnCount = 2;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(462, 275);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 3;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(211, 252);
+            this.tableLayoutPanel3.TabIndex = 3;
+            // 
+            // errorLabel
+            // 
+            this.errorLabel.AutoSize = true;
+            this.errorLabel.Location = new System.Drawing.Point(711, 43);
+            this.errorLabel.Name = "errorLabel";
+            this.errorLabel.Size = new System.Drawing.Size(91, 18);
+            this.errorLabel.TabIndex = 4;
+            this.errorLabel.Text = "Error Label";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1117, 550);
+            this.Controls.Add(this.errorLabel);
+            this.Controls.Add(this.tableLayoutPanel3);
+            this.Controls.Add(this.tableLayoutPanel2);
+            this.Controls.Add(this.resetButton);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.PictureBox pictureBox8;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Button resetButton;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.Label errorLabel;
+    }
+}
+
---
+++ AnimalSyogi/Form1.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace AnimalSyogi {
+    public partial class Form1 : Form {
+        Board board = new Board();
+        Koma chick_1;
+        Koma chick_2;
+        Koma giraffe_1;
+        Koma giraffe_2;
+        Koma elephant_1;
+        Koma elephant_2;
+        Koma lion_1;
+        Koma lion_2;
+        Point pos_init;
+        int[,] chick_move = new int[3,3] { { 0, 1, 0 }, { 0, 0, 0 }, { 0, 0, 0 } };
+        int[,] giraffe_move = new int[3, 3] { { 0, 1, 0 }, { 1, 0, 1 }, { 0, 1, 0 } };
+        int[,] elephant_move = new int[3, 3] { { 1, 0, 1 }, { 0, 0, 0 }, { 1, 0, 1 } };
+        int[,] lion_move = new int[3, 3] { { 1, 1, 1 }, { 1, 0, 1 }, { 1, 1, 1 } };
+        int[,] chicken_move = new int[3, 3] { { 1, 1, 1 }, { 1, 0, 1 }, { 0, 1, 0 } };
+
+
+        public Form1() {
+            InitializeComponent();
+            chick_1 = new Koma(pictureBox8, 0, 1, 2, chick_move);
+            chick_2 = new Koma(pictureBox4, 1, 1, 1, chick_move);
+            giraffe_1 = new Koma(pictureBox5, 0, 2, 3, giraffe_move);
+            giraffe_2 = new Koma(pictureBox1, 1, 0, 0, giraffe_move);
+            elephant_1 = new Koma(pictureBox7, 0, 0, 3, elephant_move);
+            elephant_2 = new Koma(pictureBox3, 1, 2, 0, elephant_move);
+            lion_1 = new Koma(pictureBox6, 0, 1, 3, lion_move);
+            lion_2 = new Koma(pictureBox2, 1, 1, 0, lion_move);
+
+            animal_Initialize();
+        }
+
+        private void resetButton_Click(object sender, EventArgs e) {
+            chick_1.pos_x = 1;
+            chick_1.pos_y = 2;
+            chick_2.pos_x = 1;
+            chick_2.pos_y = 1;
+            giraffe_1.pos_x = 2;
+            giraffe_1.pos_y = 3;
+            giraffe_2.pos_x = 0;
+            giraffe_2.pos_y = 0;
+            elephant_1.pos_x = 0;
+            elephant_1.pos_y = 3;
+            elephant_2.pos_x = 2;
+            elephant_2.pos_y = 0;
+            lion_1.pos_x = 1;
+            lion_1.pos_y = 3;
+            lion_2.pos_x = 1;
+            lion_2.pos_y = 0;
+            animal_Initialize();
+        }
+
+        private void resetKoma(Koma koma, int forward) {
+            tableLayoutPanel1.Controls.Add(koma.pictureBox, koma.pos_x, koma.pos_y);
+            board.append(koma);
+
+            /* 駒の向きの初期化 */
+            if (koma.forward != forward) {
+                koma.forward = forward;
+                koma.pictureBox.Image.RotateFlip(RotateFlipType.RotateNoneFlipY);
+            }
+
+            /* 駒の所有者の初期化 */
+            if (forward == 0) {
+                koma.player = 0;
+            } else if (forward == 1) {
+                koma.player = 1;
+            }
+        }
+
+        private void animal_Initialize() {
+            resetKoma(chick_1, 0);
+            resetKoma(chick_2, 1);
+            resetKoma(giraffe_1, 0);
+            resetKoma(giraffe_2, 1);
+            resetKoma(elephant_1, 0);
+            resetKoma(elephant_2, 1);
+            resetKoma(lion_1, 0);
+            resetKoma(lion_2, 1);
+            board.player = 0;
+        }
+
+        private void picture_MouseUp(object sender, MouseEventArgs e) {
+            Point pos = PointToClient(Cursor.Position);
+            int row = 0;
+            int column = 0;
+            int row_init = 0;
+            int column_init = 0;
+            double distance = 0.0;
+            Koma koma_init;
+            Koma koma_del;
+            Control pic_del;
+
+            if (pos_init.X > 3 && pos_init.X < 95) {
+                column_init = 0;
+            } else if (pos_init.X < 195) {
+                column_init = 1;
+            } else if (pos_init.X < 290) {
+                column_init = 2;
+            }
+
+            if (pos_init.Y > 10 && pos_init.Y < 96) {
+                row_init = 0;
+            } else if (pos_init.Y < 184) {
+                row_init = 1;
+            } else if (pos_init.Y < 270) {
+                row_init = 2;
+            } else if (pos_init.Y < 358) {
+                row_init = 3;
+            }
+
+            if (pos.X > 3 && pos.X < 95) {
+                column = 0;
+            } else if (pos.X < 195) {
+                column = 1;
+            } else if (pos.X < 290) {
+                column = 2;
+            }
+
+            if (pos.Y > 10 && pos.Y < 96) {
+                row = 0;
+            } else if (pos.Y < 184) {
+                row = 1;
+            } else if (pos.Y < 270) {
+                row = 2;
+            } else if (pos.Y < 358) {
+                row = 3;
+            }
+            koma_init = board.get(column_init, row_init);   //移動元の駒
+
+            /* プレーヤの手番を確認 */
+            if (koma_init.player != board.player) {
+                errorLabel.Text = "あなたの駒ではありません．";
+                return;
+            }
+
+            /* 隣接していないマスには進めない */
+            distance = Math.Sqrt(Math.Pow(row - row_init, 2) + Math.Pow(column - column_init, 2));
+            if(distance > Math.Sqrt(2) + 1e-100) {
+                errorLabel.Text = "駒の動きが間違っています．";
+                return;
+            }else if(distance < 1e-100) {
+                return;
+            }
+
+            /* 駒が移動できるマスでない場合は移動しない */
+            if (koma_init.forward == 0) {   //駒の向きで場合分け
+                if (koma_init.move[row - row_init + 1, column - column_init + 1] == 0) {
+                    errorLabel.Text = "駒の動きが間違っています．";
+                    return;
+                }
+            } else {
+                if (koma_init.move[row_init - row + 1, column_init - column + 1] == 0) {
+                    errorLabel.Text = "駒の動きが間違っています．";
+                    return;
+                }
+            }
+
+            /* 相手の駒が移動先にある場合の処理 */
+            pic_del = tableLayoutPanel1.GetControlFromPosition(column, row);
+            koma_del = board.get(column, row);
+            if (koma_del != null) {
+                if (koma_init.player == koma_del.player) {
+                    return;
+                } else {
+                    tableLayoutPanel1.Controls.Remove(pic_del);
+                    if (koma_init.player == 1) {
+                        koma_del.player = 1;    //駒の所有者を変える
+                        tableLayoutPanel2.Controls.Add(koma_del.pictureBox, 0, 0);
+                    } else if (koma_init.player == 0) {
+                        koma_del.player = 0;    //駒の所有者を変える
+                        tableLayoutPanel3.Controls.Add(koma_del.pictureBox, 0, 0);
+                        koma_del.pictureBox.Image.RotateFlip(RotateFlipType.RotateNoneFlipY);
+                        koma_del.forward = 0;
+                    }
+                    board.delete(koma_del);
+                }
+            }
+
+            board.delete(koma_init);     //元いた場所の登録は削除
+            koma_init.pos_x = column;    //盤上の位置を更新
+            koma_init.pos_y = row;       //盤上の位置を更新
+            board.append(koma_init);     //新しい盤上の位置に登録
+            tableLayoutPanel1.Controls.Add(koma_init.pictureBox, column, row);   //描画
+            board.player = (board.player + 1) % 2;  //プレイヤーの切り替え
+            errorLabel.Text = "";
+        }
+
+        private void pictureBox_MouseDown(object sender, MouseEventArgs e) {
+            if (e.Button == MouseButtons.Left) {
+                pos_init = PointToClient(Cursor.Position);
+            }
+        }
+
+        public class Board {
+            public Koma[,] board = new Koma[3, 4];
+            public int player = 0;
+
+            public void append(Koma koma) {
+                this.board[koma.pos_x, koma.pos_y] = koma;
+            }
+
+            public void delete(Koma koma) {
+                this.board[koma.pos_x, koma.pos_y] = null;
+            }
+
+            public Koma get(int x, int y) {
+                return this.board[x, y];
+            }
+        }
+
+        public class Koma {
+            public int head = 0;    //駒の表裏，表：0，裏：1
+            public int pos_x;
+            public int pos_y;
+            public int player;  //駒の所有者，手前：0，裏：1
+            public int forward = 0; //駒の向き，上：0，下：1
+            public int[,] move = new int[3, 3];
+            public System.Windows.Forms.PictureBox pictureBox;
+
+            public Koma(System.Windows.Forms.PictureBox pictureBox, int player, int pos_x, int pos_y, int[,] move) {
+                this.player = player;
+                this.pictureBox = pictureBox;
+                this.pictureBox.Visible = true;
+                this.pos_x = pos_x;
+                this.pos_y = pos_y;
+                this.move = move;
+                if (this.player == 1) {
+                    this.pictureBox.Image.RotateFlip(RotateFlipType.RotateNoneFlipY);
+                    this.pictureBox.Refresh();
+                    this.forward = 1;
+                }
+            }
+        }
+    }
+}
